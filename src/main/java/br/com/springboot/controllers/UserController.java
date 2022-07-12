3cd5c2f9a477868352d24a66cd4320c3724eadaf
Mike Andrade <mike.andrade.oliveira@gmail.com>
2022-07-12 15:57:12 -0300

    Criando os métodos userDtoToUser e userToUserDto

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -1,9 +1,9 @@
 package br.com.springboot.controllers;
 
 import br.com.springboot.model.User;
-import br.com.springboot.repositories.UserRepository;
 import br.com.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,18 +15,15 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/")
-    public String helloWorld(){
-        return "Hello world";
-    }
 
     @GetMapping
-    public List<User> findAll(){
-        return userService.findAllUsers();
+    public ResponseEntity<List<User>> findAll(){
+        List<User> users = userService.findAllUsers();
+        return ResponseEntity.ok(users);
     }
 
     @PostMapping
-    public User create(@RequestBody User user){
-        return userService.createUser(user);
+    public ResponseEntity<User> create(@RequestBody User user){
+        return ResponseEntity.ok(userService.createUser(user));
     }
 }
--- src/main/java/br/com/springboot/service/impl/UserServiceImpl.java
+++ src/main/java/br/com/springboot/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package br.com.springboot.service.impl;
 
 import br.com.springboot.model.User;
+import br.com.springboot.model.UserDto;
 import br.com.springboot.repositories.UserRepository;
 import br.com.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,23 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
 
+
+    public UserDto userToUserDto(User user){
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setName(user.getName());
+        userDto.setUsername(user.getUsername());
+
+        return userDto;
+    }
+    public User userDtoToUser(UserDto userDto){
+        User user = new User();
+        user.setId(userDto.getId());
+        user.setName(userDto.getName());
+        user.setUsername(userDto.getUsername());
+
+        return user;
+    }
     @Autowired
     private UserRepository userRepository;
 
